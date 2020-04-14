e6a18ec58079570b357d73a378efffe957d34326
Moksh <mokshdipikasikkem@msitprogram.net>
2020-04-14 22:23:19 +0530

    put method defined

--- lru.py
+++ lru.py
@@ -4,7 +4,21 @@ class lru:
         self.lru=[]
         self.cache={}
     def put(self,key):
-        pass
+        if len(self.lru) < self.capacity:
+            if key in self.lru:
+                self.lru.remove(key)
+                self.lru.append(key)  
+                self.cache[key]  = "Hello"+str(key)
+            else:
+                self.lru.append(key)
+                self.cache[key] = "Hello"+str(key)
+        else:
+            
+            r = self.lru.pop(0)
+            del self.cache[r]
+            self.lru.append(key)
+            self.cache[key] = "Hello"+ str(key)
+
     def get(self,key):
         pass
     def getcache(self):
